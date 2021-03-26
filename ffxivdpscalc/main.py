6d062e62ee740df73e3b67c2c8318b662dfbddd2
joseph-salmingo <joseph.salmingo@gmail.com>
2021-03-26 23:44:19 +0900

    moves test out and main into module

--- ffxivdpscalc/main.py
+++ ffxivdpscalc/main.py
@@ -202,8 +202,3 @@ class Comp():
       elif job.role == Roles.CASTER: has_caster = 1
     self.n_roles = has_tank + has_healer + has_melee + has_ranged + has_caster
 
-def run_test():
-  me = CharacterStats(Jobs.SCH, 180, 5577, 2272, 3802, 1100, 2139, 380, 340)
-  the_shitters_i_raid_with = Comp({Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.AST, Jobs.SAM, Jobs.NIN, Jobs.MCH, Jobs.SMN})
-  # have to manually provide pps for testing for now
-  print(me.calc_damage(139.71, the_shitters_i_raid_with))
