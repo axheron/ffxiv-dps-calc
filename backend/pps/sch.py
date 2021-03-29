5dc35bc13efe97f6ef0ce0d1cf536a5a17bea8a3
scotchkorean27 <scottlee27@hotmail.com>
2021-03-28 22:29:59 -0700

    added ed_per_min back into pps calc

--- backend/pps/sch.py
+++ backend/pps/sch.py
@@ -15,23 +15,25 @@ class SchPps(HealerPps):
         
     # hard coded for a ~180 second cycle, actual length calculated by get_cycle
     # todo: extend this for variable length
-    def total_potency_spreadsheet_port(self, character_stats, caster_tax):
+    def total_potency_spreadsheet_port(self, character_stats, caster_tax, ed_per_min, num_filler_casts):
         # do as the spreadsheet do        
         short_gcd = character_stats.get_gcd()
         
         result = 0
-        result += 12*self.ed_potency
+        result += 3 * self.ed_potency * ed_per_min
         
         sps_scalar = character_stats.get_dot_scalar()
         # 1 bio + x B3 and 4 R2s that replace B3s
-        if (((30-2*short_gcd) % (short_gcd+caster_tax)) > 1.5):
-            result += 6*(math.ceil((30-2*short_gcd)/(short_gcd+caster_tax)))*self.b3_potency+2*self.b3_potency+4*self.r2_potency
-            result += 6*10*sps_scalar*self.bio_potency
+        if (((30 - 2 * short_gcd) % (short_gcd + caster_tax)) > 1.5):
+            result += 6 * math.ceil((30 - 2 * short_gcd) / (short_gcd + caster_tax)) * self.b3_potency + 2 * self.b3_potency + 4 * self.r2_potency
+            result += 6 * 10 * sps_scalar * self.bio_potency
         else:
-            result += 6*(math.floor((30-2*short_gcd)/(short_gcd+caster_tax)))*self.b3_potency+2*self.b3_potency+4*self.r2_potency
+            result += 6 * math.floor((30 - 2 * short_gcd) / (short_gcd + caster_tax)) * self.b3_potency + 2 * self.b3_potency + 4 * self.r2_potency
             
-            result += 6*9*sps_scalar*self.bio_potency
-            result += 6*((3-((30-2*short_gcd) % (short_gcd+caster_tax)))/3)*sps_scalar*self.bio_potency
+            result += 6 * 9 * sps_scalar * self.bio_potency
+            result += 6 * ((3 - ((30 - 2 * short_gcd) % (short_gcd + caster_tax))) / 3) * sps_scalar * self.bio_potency
+            
+        result -= 3 * num_filler_casts * self.b3_potency
                 
         return result
         
--- backend/tests/testSch.py
+++ backend/tests/testSch.py
@@ -9,7 +9,7 @@ class TestSchCalc(unittest.TestCase):
     def test_total_potency(self):
         me = CharacterStats(Jobs.SCH, 180, 5577, 2272, 3802, 1100, 2139, 380, 340)
         mypps = SchPps()
-        self.assertEqual(mypps.total_potency_spreadsheet_port(me, 0.12), 24326.1264)
+        self.assertEqual(mypps.total_potency_spreadsheet_port(me, 0.12, 4, 0), 24326.1264)
     
     def test_cycle(self):
         me = CharacterStats(Jobs.SCH, 180, 5577, 2272, 3802, 1100, 2139, 380, 340)
