951aceb2701ddb258e41931526b4928453fac808
scotchkorean27 <scottlee27@hotmail.com>
2021-03-28 18:14:15 -0700

    add a comment and i guess i contributed lol

--- backend/pps/pps.py
+++ backend/pps/pps.py
@@ -6,6 +6,7 @@ from backend.calc import CharacterStats
 class Pps:
         
     # hard coded for a ~180 second cycle, actual length calculated by get_cycle
+    # character_stats is a calc.CharacterStats object
     # todo: extend this for variable length
     def calculate_pps(self, character_stats, caster_tax):
         raise NotImplementedError
