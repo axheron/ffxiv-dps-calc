4e21b4a7976310646a40a0fe962d369071941898
Juanaxu <Juanaxu@gmail.com>
2021-04-04 20:03:57 -0400

    Moved the calc_damage call into a service, with selected party comp and job

--- client/src/app/gearset-list/gearset-list.component.ts
+++ client/src/app/gearset-list/gearset-list.component.ts
@@ -4,64 +4,19 @@ import { take } from 'rxjs/operators';
 import { GearSet, GearSetResponse } from '../data/gearset';
 import { DpsService } from '../service/dps.service';
 
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from "@angular/common/http";
-
 @Component({
   selector: 'app-gearset-list',
   templateUrl: './gearset-list.component.html',
   styleUrls: ['./gearset-list.component.css']
 })
 export class GearsetListComponent implements OnInit {
-	dataSet = [
-	{
-		id: '1',
-		name: '5.4 Preliminary SCH BiS',
-		weaponDamage: 180,
-		mainStat: 5577,
-		dh: 1100,
-		crit: 3802,
-		det: 2272,
-		speed: 2139,
-		ten: 380,
-		pie: 682,
-		gcd: 2.32,
-		mp: -1191.90,
-		dps: 13535.61,
-	}, {
-		id: '2',
-		name: '5.4 Super Safe Set',
-		weaponDamage: 180,
-		mainStat: 5577,
-		dh: 1460,
-		crit: 4033,
-		det: 1995,
-		speed: 1223,
-		ten: 380,
-		pie: 1284,
-		gcd: 2.41,
-		mp: -291.03,
-		dps: 13240.73,
-	}, {
-		id: '3',
-		name: '5.45 BiS Tome Earrings',
-		weaponDamage: 180,
-		mainStat: 5510,
-		dh: 1400,
-		crit: 3781,
-		det: 2478,
-		speed: 2141,
-		ten: 380,
-		pie: 340,
-		gcd: 2.32,
-		mp: -1491.90,
-		dps: 13621.75,
-	}];
+
 	editCache: { [key: string]: { edit: boolean; data: GearSet } } = {};
 
-  constructor(private dpsService: DpsService, private httpClient: HttpClient) { }
+  constructor(public dpsService: DpsService) { }
 
   ngOnInit(): void {
-    this.getAllDamage();
+    this.dpsService.getAllDamage();
   	this.updateEditCache();
   }
 
@@ -70,53 +25,27 @@ export class GearsetListComponent implements OnInit {
   }
 
   cancelEdit(id: string): void {
-    const index = this.dataSet.findIndex(item => item.id === id);
+    const index = this.dpsService.dataSet.findIndex(item => item.id === id);
     this.editCache[id] = {
-      data: { ...this.dataSet[index] },
+      data: { ...this.dpsService.dataSet[index] },
       edit: false
     };
   }
 
   saveEdit(id: string): void {
-    const index = this.dataSet.findIndex(item => item.id === id);
-    Object.assign(this.dataSet[index], this.editCache[id].data);
+    const index = this.dpsService.dataSet.findIndex(item => item.id === id);
+    Object.assign(this.dpsService.dataSet[index], this.editCache[id].data);
     this.editCache[id].edit = false;
-    this.getDamage(index);
+    this.dpsService.getDamage(index);
   }
 
   updateEditCache(): void {
-    this.dataSet.forEach(item => {
+    this.dpsService.dataSet.forEach(item => {
       this.editCache[item.id] = {
         edit: false,
         data: { ...item }
       };
     });
-  }
-
-  private calculateDps(index: number): void {
-    this.dpsService.calculateDps(this.dataSet[index]).pipe(take(1)).subscribe((dps: number) => {
-      this.dataSet[index].dps = dps;
-    })
-
-  private getAllDamage(): void {
-  	for (let index : number = 0; index < this.dataSet.length; index++) {
-  	  this.getDamage(index);
-  	}
-  }
-
-  private getDamage(index: number): void {
-  	const damageUrl = 'https://ffxiv-dps-calc-backend.uc.r.appspot.com/calc_damage';
-  	const headers = new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-            })
-  	const player = this.dataSet[index];
-  	/* TODO: stop hardcoding comp */
-  	const comp = ['SCH', 'PLD', 'GNB', 'AST', 'MCH', 'DRG', 'MNK', 'BLM'];
-  	const response = this.httpClient.post<GearSetResponse>(damageUrl, JSON.stringify({player: player, comp: comp, job: 'SCH'}), {headers: headers}).subscribe(res => this.dataSet[index].dps = res.dps);
   }
 
 }
--- client/src/app/service/dps.service.ts
+++ client/src/app/service/dps.service.ts
@@ -3,30 +3,83 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { GearSet } from '../data/gearset';
+import { GearSet, GearSetResponse } from '../data/gearset';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DpsService {
+  selfJob = 'SCH';
+  party = ['PLD', 'GNB', 'AST', 'MCH', 'DRG', 'MNK', 'BLM'];
+  dataSet = [
+    {
+      id: '1',
+      name: '5.4 Preliminary SCH BiS',
+      weaponDamage: 180,
+      mainStat: 5577,
+      dh: 1100,
+      crit: 3802,
+      det: 2272,
+      speed: 2139,
+      ten: 380,
+      pie: 682,
+      gcd: 2.32,
+      mp: -1191.90,
+      dps: 13535.61,
+    }, {
+      id: '2',
+      name: '5.4 Super Safe Set',
+      weaponDamage: 180,
+      mainStat: 5577,
+      dh: 1460,
+      crit: 4033,
+      det: 1995,
+      speed: 1223,
+      ten: 380,
+      pie: 1284,
+      gcd: 2.41,
+      mp: -291.03,
+      dps: 13240.73,
+    }, {
+      id: '3',
+      name: '5.45 BiS Tome Earrings',
+      weaponDamage: 180,
+      mainStat: 5510,
+      dh: 1400,
+      crit: 3781,
+      det: 2478,
+      speed: 2141,
+      ten: 380,
+      pie: 340,
+      gcd: 2.32,
+      mp: -1491.90,
+      dps: 13621.75,
+    }];
 
   constructor(private http: HttpClient) { }
 
-  calculateDps(gearSet: GearSet): Observable<number> {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Accept': 'text/plain',
-        'Content-Type': 'text/plain'
-      }),
-      /* No I don't understand why this needed to be typecast. */
-      'responseType': 'text' as 'text'
-    };
-    return this.http.get('/calc_damage', httpOptions).pipe(
-      map((result) => {
-        console.log(result);
-        const numbers = result.match(/\d+/);
-        const dpsValue = numbers ? parseInt(numbers[0]) : 0;
-        return dpsValue;
-      }));
+  getAllDamage(): void {
+    for (let index : number = 0; index < this.dataSet.length; index++) {
+      this.getDamage(index);
+    }
+  }
+
+  getDamage(index: number): void {
+    const damageUrl = 'https://ffxiv-dps-calc-backend.uc.r.appspot.com/calc_damage';
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+    });
+    const request = JSON.stringify({
+        player: this.dataSet[index],
+        comp: this.party,
+        job: this.selfJob
+    });
+    this.http.post<GearSetResponse>(damageUrl, request, {headers: headers}).subscribe((res: GearSetResponse) => {
+      this.dataSet[index].dps = res.dps;
+    });
   }
 }
