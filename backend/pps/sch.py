4b0fe9567e734936bb66eca77225dbbec515dec7
scotchkorean27 <scottlee27@hotmail.com>
2021-04-06 00:45:13 -0700

    update comment

--- backend/pps/sch.py
+++ backend/pps/sch.py
@@ -30,8 +30,12 @@ class SchPps(HealerPps):
         else:
             result += 6 * math.floor((30 - 2 * short_gcd) / (short_gcd + caster_tax)) * self.b3_potency + 2 * self.b3_potency + 4 * self.r2_potency
             
-            result += 6 * 9 * sps_scalar * self.bio_potency
-            result += 6 * ((3 - ((30 - 2 * short_gcd) % (short_gcd + caster_tax))) / 3) * sps_scalar * self.bio_potency
+            total_bio = 6 * 9 * sps_scalar * self.bio_potency
+            result += total_bio
+            weird_fractional_bio_tick =  6 * ((3 - ((30 - 2 * short_gcd) % (short_gcd + caster_tax))) / 3) * sps_scalar * self.bio_potency
+            print(total_bio)
+            print(weird_fractional_bio_tick)
+            result += weird_fractional_bio_tick
             
         result -= 3 * num_filler_casts * self.b3_potency
                 
@@ -57,6 +61,7 @@ class SchPps(HealerPps):
         #    As long as the final minute is over 9 seconds (or 18 in a dissipation minute), we have no reason to believe any would be wasted
         #        Because I am lazy, I just write off all of the EDs in the last minute as a loss if you don't have time to burn all of them
         #        todo: model partial spending if the last minute is sub 20 seconds
+        #    todo: model partial ED usage (ed_per_min or non_ed_per_min or w/e)
         num_af = 3 * math.ceil((num_seconds - 10) / 60) + 3 * math.ceil((num_seconds - 20) / 180) 
         
         # The number of r2s is modeled as:
@@ -79,5 +84,7 @@ class SchPps(HealerPps):
         
         
         num_bio_ticks = math.floor(num_seconds / 3)
+        
+        print(self.bio_potency * character.get_dot_scalar() * num_bio_ticks)
         
         return self.b3_potency * num_b3 + num_r2 * (self.r2_potency) + num_swift_b3 * (self.b3_potency) + self.bio_potency * character.get_dot_scalar() * num_bio_ticks + self.ed_potency * num_af
