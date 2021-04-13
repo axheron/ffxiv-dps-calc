5d95089017f9ae42270015eba370ccd77cc5e117
Chris King <me@chrisking.io>
2021-04-13 13:45:05 -0500

    Use some ~abstraction~ and move more stuff around, much to everyone's misery

---
+++ backend/xivdpscalc/pps/__init__.py
@@ -0,0 +1,26 @@
+""" General PPS calculation interfaces """
+from abc import ABCMeta, abstractmethod
+
+class Pps(metaclass=ABCMeta):
+    """ Top level PPS calculation interface. """
+
+    @abstractmethod
+    def calculate_pps(self, character_stats, caster_tax):
+        """
+        # hard coded for a ~180 second cycle, actual length calculated by get_cycle
+        # character_stats is a backend.character.Character object
+        # todo: extend this for variable length
+        """
+        ...
+
+class HealerPps(Pps):
+    """ PPS interface geared specifically for healers """
+
+    @abstractmethod
+    def calculate_pps(self, character_stats, caster_tax):
+        """
+        # hard coded for a ~180 second cycle, actual length calculated by get_cycle
+        # character_stats is a backend.character.Character object
+        # todo: extend this for variable length
+        """
+        ...
