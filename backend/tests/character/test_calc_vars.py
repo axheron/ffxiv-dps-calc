3e6592ec00cfce5cd1304bbc91f957535dd64de8
joseph-salmingo <joseph.salmingo@gmail.com>
2021-04-16 01:03:40 +0900

    changes tests to be accurate to some relevant decimal place

--- backend/tests/character/test_calc_vars.py
+++ backend/tests/character/test_calc_vars.py
@@ -14,7 +14,7 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         the_shitters_i_raid_with = Comp(
             {Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.WHM, Jobs.SAM, Jobs.SAM, Jobs.MCH, Jobs.BLM})
         # have to manually provide pps for testing for now
-        self.assertEqual(20381.368540000003, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(20381.368540000003, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
 
     def test_applies_comp_penalty(self):
         """ 3 roles, no raid buffs, should be lower than the number in test_baseline """
@@ -24,7 +24,7 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         # 3 roles, who even needs raid buffs
         the_shitters_i_raid_with = Comp({Jobs.PLD, Jobs.WHM, Jobs.SAM})
         # have to manually provide pps for testing for now
-        self.assertEqual(19855.659200000002, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(19855.659200000002, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
 
     def test_applies_crit_dh_raid_bonuses(self):
         """ 5 roles, sch and brd for raid buffs, should be higher than test_baseline """
@@ -35,14 +35,14 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         the_shitters_i_raid_with = Comp(
             {Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.SAM, Jobs.BRD, Jobs.BLM})
         # have to manually provide pps for testing for now
-        self.assertEqual(20685.98776726701, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(20685.98776726701, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
 
     def test_gcd(self):
         """ Test gcd is applying speed correctly """
         my_stat_spread = CharacterStatSpread(
             wd=180, mainstat=5577, det=2272, crit=3802, dh=1100, speed=2139, ten=380, pie=340)
         test_char = Character(Jobs.SCH, my_stat_spread)
-        self.assertEqual(2.32, test_char.get_gcd())
+        self.assertAlmostEqual(2.32, test_char.get_gcd(), places=2)
 
     def test_base_piety(self):
         """ Test applying piety correctly """
@@ -56,7 +56,7 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         my_stat_spread = CharacterStatSpread(
             wd=180, mainstat=5577, det=2272, crit=3802, dh=1100, speed=2139, ten=380, pie=340)
         test_char = Character(Jobs.SCH, my_stat_spread)
-        self.assertEqual(1.069, test_char.get_dot_scalar())
+        self.assertAlmostEqual(1.069, test_char.get_dot_scalar(), places=3)
 
     def test_single_raidbuff(self):
         """ Test raid buff is applying damage correctly """
@@ -66,7 +66,7 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         the_shitters_i_raid_with = Comp(
             {Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.AST, Jobs.SAM, Jobs.SAM, Jobs.MCH, Jobs.BLM})
         # have to manually provide pps for testing for now
-        self.assertEqual(20534.228804050006, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(20534.228804050006, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
 
     def test_multiple_raidbuff(self):
         """ Test multiple raid buffs are applying damage correctly """
@@ -76,7 +76,7 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         the_shitters_i_raid_with = Comp(
             {Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.AST, Jobs.NIN, Jobs.SAM, Jobs.MCH, Jobs.BLM})
         # have to manually provide pps for testing for now
-        self.assertEqual(20790.90666410063, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(20790.90666410063, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
 
     def test_embolden_has_no_effect(self):
         """ Test embolden isn't mistakenly applying damage """
@@ -86,4 +86,4 @@ class TestCalcVars(unittest.TestCase): #pylint: disable=missing-class-docstring
         the_shitters_i_raid_with = Comp(
             {Jobs.PLD, Jobs.WAR, Jobs.SCH, Jobs.WHM, Jobs.SAM, Jobs.SAM, Jobs.MCH, Jobs.RDM})
         # have to manually provide pps for testing for now
-        self.assertEqual(20381.368540000003, test_char.calc_damage(200, the_shitters_i_raid_with))
+        self.assertAlmostEqual(20381.368540000003, test_char.calc_damage(200, the_shitters_i_raid_with), places=3)
