6e81d18e2331efd8b89a98a102332f8f27fa6d1b
joseph-salmingo <joseph.salmingo@gmail.com>
2021-04-19 19:45:30 +0900

    undoes changes to calc_damage

--- backend/xivdpscalc/character/__init__.py
+++ backend/xivdpscalc/character/__init__.py
@@ -32,7 +32,6 @@ class Character:
         """
         self.job = job
         self.wd = stat_spread.wd
-        # TODO: separate probablistic stats from regular stats
         self.character_stats = {
             Stats.MAINSTAT: Stat(Stats.MAINSTAT, stat_spread.mainstat),
             Stats.DET: Stat(Stats.DET, stat_spread.det),
@@ -64,18 +63,21 @@ class Character:
         """
         return 200 + self.character_stats[Stats.PIE].get_multiplier()
 
-    def calc_non_probablistic_damage(self, potency_per_second: float, comp: Optional[Comp],
-                                     is_dot: bool = False) -> float:
+    # todo: break this up neatly
+    def calc_damage(self, potency_per_second: float, comp: Comp, is_dot: bool = False,
+                    crit_rate: Optional[float] = None, dh_rate: Optional[float] = None) -> float:  #  pylint: disable=too-many-arguments, too-many-locals
         """
         Calculates the damage from non probablistic stats.
         :param potency_per_second: From potency calculated on expected rotation
         :param comp: Team composition, if applicable
         :param is_dot: If potency also applies to DoT
+        :param crit_rate: optional parameter to override crit rate from gear
+        :param dh_rate: optional parameter to override crit rate from gear
         :returns: DPS contribution from non-probablistic stats
         """
 
         # modify mainstat according to number of roles if with a team composition
-        if comp:
+        if comp is not None:
             unique_role_bonus = 1 + 0.01 * comp.n_roles
             new_mainstat_value = math.floor(self.character_stats[Stats.MAINSTAT].value * unique_role_bonus)
             mainstat = Stat(Stats.MAINSTAT, new_mainstat_value)
@@ -84,31 +86,22 @@ class Character:
 
         # job bonus for weapon damage
         adjusted_wd = self.wd + (340 * self.job.job_mod // 1000)
-        np_damage = potency_per_second * adjusted_wd * (100 + mainstat.get_multiplier()) // 100
+        damage = potency_per_second * adjusted_wd * (100 + mainstat.get_multiplier()) // 100
 
-        # applying the damage bonus from wimpy secondary stats
-        np_damage = self.character_stats[Stats.DET].apply_stat(np_damage)
-        np_damage = self.character_stats[Stats.TEN].apply_stat(np_damage)
+        # applying the damage bonus from secondary stats
+        damage = self.character_stats[Stats.DET].apply_stat(damage)
+        damage = self.character_stats[Stats.TEN].apply_stat(damage)
         if is_dot:
-            np_damage = self.character_stats[Stats.SPEED].apply_stat(np_damage)
+            damage = self.character_stats[Stats.SPEED].apply_stat(damage)
 
-        return np_damage
+        damage //= 100  # why? i do not know. cargo culted
 
-    def calc_probablistic_damage(self, np_damage: float, comp: Optional[Comp] = None,
-                                 crit_rate: Optional[float] = None,
-                                 dh_rate: Optional[float] = None) -> float:
-        """
-        Calculates the damage from probablistic stats.
-        :param np_damage: from nonprobablistic damage calculation
-        :param comp: Team composition, if applicable
-        :param crit_rate: for calculating a specific crit proc rate
-        :param dh_rate: for calculating a specific dh proc rate
-        :returns: DPS contribution from probablistic stats
-        """
+        if self.job.role == Roles.HEALER:
+            damage = math.floor(damage * 1.3)  # magic and mend
 
         # damage effect of probabalistic stats
-        crit_damage = self.character_stats[Stats.CRIT].apply_stat(np_damage)
-        dh_damage = np_damage * self.character_stats[Stats.DH].stat.m_factor // 1000
+        crit_damage = self.character_stats[Stats.CRIT].apply_stat(damage)
+        dh_damage = damage * self.character_stats[Stats.DH].stat.m_factor // 1000
         cdh_damage = crit_damage * self.character_stats[Stats.DH].stat.m_factor // 1000
 
         # use expected crit rate based on stats if none is supplied
@@ -130,39 +123,15 @@ class Character:
         assert dh_rate is not None  # for mypy
         cdh_rate = crit_rate * dh_rate
         normal_rate = 1 - crit_rate - dh_rate + cdh_rate
-        p_damage = np_damage * normal_rate + (
+        damage = damage * normal_rate + (
             crit_damage * (crit_rate - cdh_rate)) + (
             dh_damage * (dh_rate - cdh_rate)) + (
             cdh_damage * cdh_rate)
-
-        return p_damage
-
-    # todo: break this up neatly
-    def calc_damage(self, potency_per_second: float, comp: Optional[Comp] = None,  # pylint: disable=too-many-arguments
-                    is_dot: bool = False, crit_rate: float = None, dh_rate: float = None) -> float:
-        """
-        Calculates the estimated DPS based on the team composition and current character stats
-        :param potency_per_second: Potency calculated on expected rotation
-        :param comp: Team composition
-        :param is_dot: The potency is applying to a DoT effect, which is additionally modified by spellspeed
-        :param crit_rate: Optional parameter to override crit rate to calculate specific proc rate
-        :param dh_rate: Optional parameter to override dh rate to calculate specific proc rate
-        :returns: the DPS number
-        """
-        damage = self.calc_non_probablistic_damage(potency_per_second, comp, is_dot)
-        damage //= 100  # why? i do not know. cargo culted
-
-        # damage effect of job traits / stance
-        # todo: pull out traits
-        if self.job.role == Roles.HEALER:
-            damage = math.floor(damage * 1.3)  # magic and mend
-
-        total_damage = self.calc_probablistic_damage(damage, comp, crit_rate, dh_rate)
 
         # apply raid buffs that boost damage
         if comp is not None:
             for buff in comp.raidbuffs:
                 if buff in Buffs.damage_buffs():
-                    total_damage *= (1 + buff.avg_buff_effect(self.job))
+                    damage *= (1 + buff.avg_buff_effect(self.job))
 
-        return total_damage
+        return damage
