63a8b00769dd1037d8a8bf26cda1ef5e3e49a054
Chris King <me@chrisking.io>
2021-04-21 09:54:57 -0500

    Add more db. to everything

--- backend/xivdpscalc/models/tags.py
+++ backend/xivdpscalc/models/tags.py
@@ -8,23 +8,23 @@ class Tag(db.Model):
   __tablename__ = 'tags'
 
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(TAG_NAME_MAX_LENGTH)
-  color = db.Column(db.String) # hex coloring in the UI
+  name = db.Column(db.String(TAG_NAME_MAX_LENGTH))
+  color = db.Column(db.String(HEX_COLOR_LENGTH)) # hex coloring in the UI
 
 class HasTags:
-  @declared_attr
+  @db.declared_attr
   def tags(cls):
-    tag_association = Table(
+    tag_association = db.Table(
       f'{cls.__tablename__}_tags',
       cls.metadata,
-      Column(
+      db.Column(
         'tag_id',
-        ForeignKey('tags.id'),
+        db.ForeignKey('tags.id'),
         primary_key=True
       ),
-      Column(
+      db.Column(
         f'{cls.__tablename__}_id',
-        ForeignKey(f'{cls.__tablename__}.id')
+        db.ForeignKey(f'{cls.__tablename__}.id')
       )
     )
-    return relationship(Tag, secondary=tag_association)
+    return db.relationship(Tag, secondary=tag_association)
