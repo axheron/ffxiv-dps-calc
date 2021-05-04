437d488259cb681455c49f59eb50d4a862adb7e7
Juanaxu <Juanaxu@gmail.com>
2021-05-04 19:43:23 -0400

    Accessibility fixes to the UI (#92)
    Wrapped every clickable thing in a <button></button> element so they're focusable, screen readers know they're clickable, and have basic browser accessibility functionality.
    Added alt text to buff images.
    Added label text to the inputs when editing a gearset.
    
    Co-authored-by: Al Kao <3639365+ackao@users.noreply.github.com>

--- client/src/app/data/job.ts
+++ client/src/app/data/job.ts
@@ -1,107 +1,125 @@
 export interface Job {
+  id: string;
   name: string;
   path: string;
   buffs: string[];
 }
 
-export const ASTROLOGIAN: Job = {
-  name: 'AST',
+const ASTROLOGIAN: Job = {
+  id: 'AST',
+  name: 'Astrologian',
   path: './assets/astrologian.png',
   buffs: ['DIVINATION'],
 };
 
-export const BARD: Job = {
-  name: 'BRD',
+const BARD: Job = {
+  id: 'BRD',
+  name: 'Bard',
   path: './assets/bard.png',
   buffs: ['BATTLE_VOICE'],
 };
 
-export const BLACK_MAGE: Job = {
-  name: 'BLM',
+const BLACK_MAGE: Job = {
+  id: 'BLM',
+  name: 'Black mage',
   path: './assets/blackmage.png',
   buffs: [],
 };
 
-export const DANCER: Job = {
-  name: 'DNC',
+const DANCER: Job = {
+  id: 'DNC',
+  name: 'Dancer',
   path: './assets/dancer.png',
   buffs: ['TECHNICAL_FINISH'],
 };
 
-export const DARK_KNIGHT: Job = {
-  name: 'DRK',
+const DARK_KNIGHT: Job = {
+  id: 'DRK',
+  name: 'Dark knight',
   path: './assets/darkknight.png',
   buffs: [],
 };
 
-export const DRAGOON: Job = {
-  name: 'DRG',
+const DRAGOON: Job = {
+  id: 'DRG',
+  name: 'Dragonn',
   path: './assets/dragoon.png',
   buffs: ['BATTLE_LITANY'],
 };
 
-export const GUNBREAKER: Job = {
-  name: 'GNB',
+const GUNBREAKER: Job = {
+  id: 'GNB',
+  name: 'Gunbreaker',
   path: './assets/gunbreaker.png',
   buffs: [],
 };
 
-export const MACHINIST: Job = {
-  name: 'MCH',
+const MACHINIST: Job = {
+  id: 'MCH',
+  name: 'Machinist',
   path: './assets/machinist.png',
   buffs: [],
 };
 
-export const MONK: Job = {
-  name: 'MNK',
+const MONK: Job = {
+  id: 'MNK',
+  name: 'Monk',
   path: './assets/monk.png',
   buffs: ['BROTHERHOOD'],
 };
 
-export const NINJA: Job = {
-  name: 'NIN',
+const NINJA: Job = {
+  id: 'NIN',
+  name: 'Ninja',
   path: './assets/ninja.png',
   buffs: ['TRICK_ATTACK'],
 };
 
-export const PALADIN: Job = {
-  name: 'PLD',
+const PALADIN: Job = {
+  id: 'PLD',
+  name: 'Paladin',
   path: './assets/paladin.png',
   buffs: [],
 };
 
-export const RED_MAGE: Job = {
-  name: 'RDM',
+const RED_MAGE: Job = {
+  id: 'RDM',
+  name: 'Red mage',
   path: './assets/redmage.png',
   buffs: ['EMBOLDEN'],
 };
 
-export const SAMURAI: Job = {
-  name: 'SAM',
+const SAMURAI: Job = {
+  id: 'SAM',
+  name: 'Samurai',
   path: './assets/samurai.png',
   buffs: [],
 };
 
-export const SCHOLAR: Job = {
-  name: 'SCH',
+const SCHOLAR: Job = {
+  id: 'SCH',
+  name: 'Scholar',
   path: './assets/scholar.png',
   buffs: ['CHAIN_STRATAGEM'],
 };
 
-export const SUMMONER: Job = {
-  name: 'SMN',
+const SUMMONER: Job = {
+  id: 'SMN',
+  name: 'Summoner',
   path: './assets/summoner.png',
   buffs: ['DEVOTION'],
 };
 
-export const WARRIOR: Job = {
-  name: 'WAR',
+const WARRIOR: Job = {
+  id: 'WAR',
+  name: 'Warrior',
   path: './assets/warrior.png',
   buffs: [],
 };
 
-export const WHITE_MAGE: Job = {
-  name: 'WHM',
+const WHITE_MAGE: Job = {
+  id: 'WHM',
+  name: 'White mage',
   path: './assets/whitemage.png',
   buffs: [],
 };
