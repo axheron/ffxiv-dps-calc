8649c456bce41b5a61c27ac79869b8c9f6151eea
Aleksander Heron <axheron@protonmail.com>
2021-05-27 16:19:06 +1000

    Add flooring pipe

---
+++ client/src/app/pipes/floor.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+	name: 'floor'
+})
+
+export class FloorPipe implements PipeTransform {
+	transform(value: number): number {
+		return Math.floor(value);
+	}
+}
